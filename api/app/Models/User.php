539c3590783027db93093599d81c3d0a85cbdb4c
Paolo Bellini <paolobellini.web@gmail.com>
2026-03-19 22:22:11 +0100

    wip: update user model and remove useless migrations

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -6,18 +6,18 @@ namespace App\Models;
 
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'tenant_id'])]
-#[Hidden(['password', 'remember_token'])]
-final class User extends Authenticatable
+final class User extends Model
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+
+    public $incrementing = false;
+
+    protected $keyType = 'string';
 
     /**
      * @return BelongsTo<Tenant, $this>
@@ -25,16 +25,5 @@ final class User extends Authenticatable
     public function tenant(): BelongsTo
     {
         return $this->belongsTo(Tenant::class);
-    }
-
-    /**
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
     }
 }
--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -6,34 +6,23 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends Factory<User>
  */
 final class UserFactory extends Factory
 {
-    protected static ?string $password;
-
     /**
      * @return array<string, mixed>
      */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'id' => fake()->uuid(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => self::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'company_name' => fake()->optional()->company(),
         ];
-    }
-
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
