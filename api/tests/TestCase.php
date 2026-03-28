4011807ea24861cebf4a38853b160cbd5cef5461
Paolo Bellini <paolobellini.web@gmail.com>
2026-03-28 13:13:20 +0100

    wip: handle secondary db connection for tests

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests;
 
 use App\Models\Tenant;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Artisan;
 
@@ -13,6 +14,8 @@ abstract class TestCase extends BaseTestCase
     protected bool $tenancy = false;
 
     protected Tenant $tenant;
+
+    protected User $user;
 
     protected function setUp(): void
     {
@@ -38,7 +41,25 @@ abstract class TestCase extends BaseTestCase
     protected function initializeTenancy(): void
     {
         $this->tenant = Tenant::query()->create();
+        $this->tenant->domains()->create(['domain' => 'test.localhost']);
+        $this->user = User::factory()->create(['tenant_id' => $this->tenant->id]);
 
         tenancy()->initialize($this->tenant);
+    }
+
+    protected function tenantUrl(string $path): string
+    {
+        return 'http://test.localhost'.$path;
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    protected function tenantHeaders(): array
+    {
+        return [
+            'X-User-Id' => (string) $this->user->id,
+            'X-Tenant-Id' => $this->tenant->id,
+        ];
     }
 }
